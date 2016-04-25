fe326b94ddac098158940ee82c71c439003061a0
ohhheyyyy <ashley.hartt@gmail.com>
2016-04-25 14:18:52 -0400

    Complete bubble sort

--- bubblesort.js
+++ bubblesort.js
@@ -1,10 +1,20 @@
 function bubbleSort(array) {
-
-  for (var i = 0; i < array.length; i++) {
-    var currentItem = array[i];
-    if(currentItem < array[i]){
-      
+    var tempItem;
+    // loop over each item in the array
+    for (i = 0; i < array.length; i++) {
+        // check if current item is greater than next item
+        if (array[i] > array[i + 1]) {
+            // if it is, assign the next item to tempItem var
+            tempItem = array[i + 1];
+            // make the next item the current item, i.e. swap them
+            array[i + 1] = array[i];
+            // make current item the temp item
+            array[i] = tempItem;
+            // since we've swapped items, make i return two indices
+            i = i - 2;
+        }
     }
-  }
-
+    return array;
 }
+
+console.log(bubbleSort([3, 1, 9, 23, 24, 90, 0]));
