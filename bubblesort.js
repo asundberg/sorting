4ad584aad21e8a5c8fdb59b2632211edba2867c0
ohhheyyyy <ashley.hartt@gmail.com>
2016-04-25 13:06:51 -0400

    Add initial testspecs and bubblesort function outline

---
+++ bubblesort.js
@@ -0,0 +1,10 @@
+function bubbleSort(array) {
+
+  for (var i = 0; i < array.length; i++) {
+    var currentItem = array[i];
+    if(currentItem < array[i]){
+      
+    }
+  }
+
+}
---
+++ bubblesort.spec.js
@@ -0,0 +1,13 @@
+describe('Bubble Sort', function(){
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+
+  it('handles an array with one item', function(){
+    expect( bubbleSort([1]) ).toEqual( [1] );
+  });
+
+  it('handles an array with multiple items', function(){
+    expect( bubbleSort([3, 2, 1]) ).toEqual( [1, 2, 3] );
+  });
+});
